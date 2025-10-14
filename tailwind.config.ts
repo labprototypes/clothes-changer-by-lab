f8090eea7eb120fcde84e66aec0b0bffb78d8a53
labprototypes <hello@prototypes.agency>
2025-10-14 17:26:39 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,9 @@ import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}"
+  "./app/**/*.{ts,tsx}",
+  "./components/**/*.{ts,tsx}",
+  "./styles/**/*.css"
   ],
   theme: {
     extend: {
